afc2f673ec07b7a05171b0acee71fa7c4e39af3e
fedeabihaggle_taptap <federico.abihaggle@taptapdigital.com>
2022-11-05 09:45:43 +0100

    use dict to manage values, make the comparison sorting the results

--- pokemon_fight.py
+++ pokemon_fight.py
@@ -1,29 +1,44 @@
 from common_utils import get_args
 import pypokedex
+import sys
 
 def main():
     args = get_args()
 
     pokemons    =   {
-        0: [args.pokemon1, 0],
-        1: [args.pokemon2, 0]
+        args.pokemon1.capitalize(): 0,
+        args.pokemon2.capitalize(): 0
     }
 
-    print(f"Starting the fight: {pokemons[0][0]} vs {pokemons[1][0]}")
-
-    for _, value in pokemons.items():
-        print(value[1])
+    for key in pokemons.keys():
         try:
-            poke        =   pypokedex.get(name=value[0])
-            value[1]    =   poke.base_stats.attack
+            poke            =   pypokedex.get(name=key)
+            pokemons[key]   =   poke.base_stats.attack
         except Exception as e:
-            print(e)
-    
-    print(pokemons)
+            print(f'{e}: {key}')
+            sys.exit(1)
 
+    print(f'\nStarting the fight:')
 
+    if len(pokemons) < 2:
+        print(f'{key} ({pokemons[key]}) vs {key} ({pokemons[key]})')
 
+        poke_winner     =   key
 
+    else:
+        for key in pokemons.keys():
+            print(f'{key} ({pokemons[key]})',end=' ')
+            if key != list(pokemons.keys())[-1]:
+                print('vs',end=' ')
+            else:
+                print('\n')
+        
+        poke_winner     =   [key for key, value in pokemons.items() if value == max(pokemons.values())]
+
+    if len(poke_winner) >= 2:
+        print('\nThe battle ends in a draw.')
+    else:
+        print(f'The winner is {poke_winner[0]}!!')
 
 if __name__ == '__main__':
     main()
