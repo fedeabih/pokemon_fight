2581e5d1a8ddab178f2dcd479776ed3de391916a
fedeabihaggle_taptap <federico.abihaggle@taptapdigital.com>
2022-11-05 08:16:57 +0100

    use pypokedex

--- pokemon_fight.py
+++ pokemon_fight.py
@@ -1,12 +1,29 @@
 from common_utils import get_args
+import pypokedex
 
 def main():
     args = get_args()
 
-    pokemon1    =   args.pokemon1
-    pokemon2    =   args.pokemon2
+    pokemons    =   {
+        0: [args.pokemon1, 0],
+        1: [args.pokemon2, 0]
+    }
 
-    print(f"Starting the fight: {pokemon1} vs {pokemon2}")
+    print(f"Starting the fight: {pokemons[0][0]} vs {pokemons[1][0]}")
+
+    for _, value in pokemons.items():
+        print(value[1])
+        try:
+            poke        =   pypokedex.get(name=value[0])
+            value[1]    =   poke.base_stats.attack
+        except Exception as e:
+            print(e)
+    
+    print(pokemons)
+
+
+
+
 
 if __name__ == '__main__':
     main()
